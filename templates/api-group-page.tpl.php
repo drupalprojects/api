edd6425482a3e9bf9598da6ea06d2d8623afaf9b
Jennifer Hodgdon <yahgrp@poplarware.com>
2014-08-27 15:52:37 -0700

    Issue #2309179 by jhodgdon: Display ingroup relationships from parent to child topic

--- templates/api-group-page.tpl.php
+++ templates/api-group-page.tpl.php
@@ -8,6 +8,7 @@
  * - $alternatives: List of alternate versions (branches) of this group.
  * - $documentation: Documentation from the comment header of the constant.
  * - $see: Related API objects.
+ * - $related_topics: Related topics documentation.
  * - $objects: Formatted list of member objects, if any.
  * - $defined: HTML reference to file that defines this group.
  *
@@ -22,6 +23,11 @@
 <?php if (!empty($see)) { ?>
   <h3><?php print t('See also') ?></h3>
   <?php print $see ?>
+<?php } ?>
+
+<?php if (!empty($related_topics)) { ?>
+<h3><?php print t('Parent topics') ?></h3>
+<?php print $related_topics ?>
 <?php } ?>
 
 <h3><?php print t('File'); ?></h3>
