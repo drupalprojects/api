61e070c99ccbf33a6c144d18ca4dfb3ceb527f4d
Jennifer Hodgdon <yahgrp@poplarware.com>
2015-01-26 12:46:29 -0800

    Issue #2400355 by jhodgdon: Whitespace settings changes in Grammar parser to support new 5.4 array syntax

--- tests/special_files/php54.php
+++ tests/special_files/php54.php
@@ -9,9 +9,20 @@
  * Tests that PHP 5.4 array syntax can be parsed and displayed.
  */
 function array_5_4() {
-  // For testing PHP 5.4 array syntax.
+  // New array syntax.
   $foo = [
     'href' => 'bar',
     [[ 'nested' => 'valid']],
   ];
+
+  // Use of [] for array indexing.
+  $foo = $my_array[$i + 7]['bar'][some_function_call('foo', 'bar', 'baz')][3];
+
+  // Mixture of old and new array syntax.
+  $foo = array(
+    'bar' => 'baz',
+    'boohoo',
+    ['mixing' => 'bad', 'but' => 'legal'],
+    array(1, 3, 5),
+  );
 }
